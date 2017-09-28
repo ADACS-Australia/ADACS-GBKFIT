42149cf442b047dd9f93d116bfd1a84383b88c32
Shibli Saleheen <ssaleheen@swin.edu.au>
2017-09-28 17:22:27 +1000

    fix email telalate

--- gbkfit_web/mailer/templates.py
+++ gbkfit_web/mailer/templates.py
@@ -7,8 +7,9 @@ VERIFY_EMAIL_ADDRESS['message'] = '<p>Dear {{title}} {{first_name}} {{last_name}
                                   '<p><a href="{{link}}" target="_blank">{{link}}</a></p>' \
                                   '<p>If you believe that the email has been sent by mistake or you have not ' \
                                   'requested for an account please <strong>do not</strong> click on the link.</p>' \
-                                  '<p>Alternatively you can report this incident to <a mailto="support@gbkfit.com">' \
-                                  'support@gbkfit.com</a> for investigation.</p>' \
+                                  '<p>Alternatively you can report this incident to <a ' \
+                                  'href="mailto:support@gbkfit.com" target="_top">support@gbkfit.com</a> for ' \
+                                  'investigation.</p>' \
                                   '<p>&nbsp;</p>' \
                                   '<p>Regards,</p>' \
                                   '<p>GBKFIT-Cloud Team</p>'
