994c462b6da2c9d2eaded88b00b2a118c1eea3db
Shibli Saleheen <ssaleheen@swin.edu.au>
2017-09-28 19:25:23 +1000

    redirect authenticated user from login page to my profile

--- gbkfit_web/urls.py
+++ gbkfit_web/urls.py
@@ -6,7 +6,9 @@ from gbkfit_web.views import account, index, job, verify
 urlpatterns = [
     url(r'^register/$', account.registration, name='register'),
     url(r'^new_job/$', job.start, name='job_start'),
-    url(r'^accounts/login/$', auth_views.login, {'template_name': 'accounts/login.html'}, name='login'),
+    url(r'^accounts/login/$',
+        auth_views.LoginView.as_view(redirect_authenticated_user=True, template_name='accounts/login.html'),
+        name='login'),
     url(r'^accounts/logout/$', auth_views.logout, {'next_page': '/'}, name='logout'),
     url(r'^accounts/profile/$', account.profile, name='profile'),
     url(r'^verify/$', verify.verify, name='verify'),
