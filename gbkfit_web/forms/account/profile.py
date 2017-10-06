c33ffdd353bc876bfc4971a195c548d0d28357c5
macrocosme <danyvohl@hotmail.com>
2017-10-06 15:01:35 +1100

    Added change password page + functionality fix for password reset
    Extending django's built-in reset password and modify password functions.
    This is done in the account view/form + accounts templates

--- gbkfit_web/forms/account/profile.py
+++ gbkfit_web/forms/account/profile.py
@@ -1,5 +1,7 @@
 from django import forms
 from gbkfit_web.models import User
+from django.contrib.auth import password_validation
+from django.utils.translation import ugettext_lazy as _
 
 class EditProfileForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
@@ -48,3 +50,83 @@ class EditProfileForm(forms.ModelForm):
                 attrs={'rows': 3, 'class': 'form-control', 'tabindex': '10'},
             ),
         }
+
+
+
+class SetPasswordForm(forms.Form):
+    """
+    A form that lets a user change set their password without entering the old
+    password
+    """
+    error_messages = {
+        'password_mismatch': _("The two password fields didn't match."),
+    }
+    new_password1 = forms.CharField(
+        label=_("New password"),
+        widget=forms.PasswordInput,
+        strip=False,
+        help_text=password_validation.password_validators_help_text_html(),
+    )
+    new_password2 = forms.CharField(
+        label=_("New password confirmation"),
+        strip=False,
+        widget=forms.PasswordInput,
+    )
+
+    def __init__(self, user, *args, **kwargs):
+        self.user = user
+        super(SetPasswordForm, self).__init__(*args, **kwargs)
+        try:
+            self.fields['old_password'].widget.attrs.update({'class': "form-control"})
+        except:
+            pass
+        self.fields['new_password1'].widget.attrs.update({'class': "form-control"})
+        self.fields['new_password2'].widget.attrs.update({'class': "form-control"})
+
+    def clean_new_password2(self):
+        password1 = self.cleaned_data.get('new_password1')
+        password2 = self.cleaned_data.get('new_password2')
+        if password1 and password2:
+            if password1 != password2:
+                raise forms.ValidationError(
+                    self.error_messages['password_mismatch'],
+                    code='password_mismatch',
+                )
+        password_validation.validate_password(password2, self.user)
+        return password2
+
+    def save(self, commit=True):
+        password = self.cleaned_data["new_password1"]
+        self.user.set_password(password)
+        if commit:
+            self.user.save()
+        return self.user
+
+
+class PasswordChangeForm(SetPasswordForm):
+    """
+    A form that lets a user change their password by entering their old
+    password.
+    """
+    error_messages = dict(SetPasswordForm.error_messages, **{
+        'password_incorrect': _("Your old password was entered incorrectly. Please enter it again."),
+    })
+    old_password = forms.CharField(
+        label=_("Old password"),
+        strip=False,
+        widget=forms.PasswordInput(attrs={'autofocus': True}),
+    )
+
+    field_order = ['old_password', 'new_password1', 'new_password2']
+
+    def clean_old_password(self):
+        """
+        Validates that the old_password field is correct.
+        """
+        old_password = self.cleaned_data["old_password"]
+        if not self.user.check_password(old_password):
+            raise forms.ValidationError(
+                self.error_messages['password_incorrect'],
+                code='password_incorrect',
+            )
+        return old_password
