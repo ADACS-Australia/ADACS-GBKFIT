ee0dda3a90ee24e32ecebc10604335ddbf3f02be
macrocosme <danyvohl@hotmail.com>
2017-10-23 14:27:42 +1100

    Header: gbkfit redirects to home

--- gbkfit_web/views/index.py
+++ gbkfit_web/views/index.py
@@ -3,12 +3,6 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 def index(request):
-    # if request.user.is_authenticated():
-    #     return render(
-    #         request,
-    #         "base/base.html",
-    #     )
-    # else:
     return render(
         request,
         "base/welcome.html",
